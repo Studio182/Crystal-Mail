307e69742451643046a8998d75060fa6a17dd74b
Hunter Dolan <hunterhdolan@gmail.com>
2010-10-25 20:54:19 -0500

    Removed some unfinished work

--- plugins/google_contacts/SQL/mysql.initial.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE google_contacts LIKE contacts;
-
-ALTER TABLE `google_contacts`
-  ADD CONSTRAINT `google_contacts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
