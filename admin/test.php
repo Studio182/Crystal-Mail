781d219141fe4ac396ad92c926eec28e4fdac460
Hunter Dolan <hunterhdolan@gmail.com>
2010-10-30 17:20:03 -0500

    a few bug fixes

--- admin/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$version = "1.1";
-$infoversion = "1.0";
-           if ($version < $infoversion)
-        {
-echo "Bigger";
-} else {echo "smaller";}
-?>
--- temp/Studio182-Crystal-Mail-4bb9fd0/program/js/tiny_mce/themes/simple/skins/o2k7/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("HTTP/1.0 404 Not Found");
-echo "<h2>404 Page Not Found</h2>";
-?>
