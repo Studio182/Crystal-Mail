0482261868aa334ec38752a583321e881afc17fe
Hunter Dolan <Hunterhdolan@gmail.com>
2010-12-12 09:59:49 -0600

    Rebase Part:1

--- api/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-//We will never stop loading!!! HAHA HACKERS!
-header('WWW-Authenticate: Negotiate');
-header('WWW-Authenticate: NTLM', false);
-?>
--- api/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$_POST = $_GET;
-
-
-echo $_POST['test'];
